4c919562da683eb949a69640595721bf1988bb88
joao84ads <joaolopesxl@hotmail.com>
2025-09-23 17:33:36 -0300

    Aula 23/09/2025 Primeiro commit projeto final

---
+++ Projeto_Final/ProjetoFinal/src/main/java/com/ProjetoFinal/app/DataConfig.java
@@ -0,0 +1,22 @@
+package com.ProjetoFinal.app;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DataConfig {
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/db_java");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+
+}
